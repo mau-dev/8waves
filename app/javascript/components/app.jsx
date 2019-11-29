7528604040f1c1c6df30e3ef8e648b1826ab54a9
Maria Dimitrijevic <maria.dimitrijevikj@gmail.com>
2019-11-29 17:32:32 +0800

    linking the App, sequencer and Grid component

--- app/javascript/components/app.jsx
+++ app/javascript/components/app.jsx
@@ -1,31 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Grid from '../components/grid'
+import React from "react";
+import ReactDOM from "react-dom";
 import Sequencer from "./sequencer";
-import PlayerProvider from "./player-provider";
-import Tone from "tone";
-
-// import axios from 'axios';
-// import Grid from '@material-ui/core/Grid';
-// import { hot } from 'react-hot-loader';
 import styles from './app.scss';
 
 
+// import "./styles.css";
 
 class App extends React.Component {
-
     render() {
-
-            return (
-                <div class="wrapper sequencer">
-                <Grid />
-
-
-               </div>
-
-        );
-    }
-
+  return (
+    <div className = "flex sequencer">
+  <h3 className="text-white">hello from app component</h3>
+ <Sequencer/>
+  </div>
+  );
 }
+}
+
 
 export default App;
--- app/javascript/components/sequencer.jsx
+++ app/javascript/components/sequencer.jsx
@@ -1,67 +1,126 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-// import classnames from 'classnames';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// // import classnames from 'classnames';
 
-// import KeyBehaviour from 'keyBehaviour';
-// import Grid from 'grid';
-
-
-// import styles from './style.scss';
-
-// const cx = classnames.bind(styles)
+// // import KeyBehaviour from 'keyBehaviour';
+import Grid from './grid';
 
 
-const waves = [{
-        wave: 'kick',
-        url: 'sounds/kick.wav'
-    },
-    {
-        wave: 'tom-hi',
-        url: 'sounds/tom-hi.wav'
-    },
-    {
-        wave: 'tom-low',
-        url: 'sounds/tom-low.wav'
-    },
-    {
-        wave: 'snare',
-        url: 'sounds/snare.wav'
-    },
-    {
-         wave: 'rim',
-        url: 'sounds/rim.wav'
-    },
-    {
-         wave: 'cymbal',
-        url: 'sounds/cymbal.wav'
-    },
-    {
-         wave: 'shaker',
-        url: 'sounds/shaker.wav'
-    },
-    {
-         wave: 'snare',
-        url: 'sounds/snare.wav'
-    }
+// // import styles from './style.scss';
 
-];
+// // const cx = classnames.bind(styles)
 
 
+// const waves = [{
+//         wave: 'kick',
+//         url: 'sounds/kick.wav'
+//     },
+//     {
+//         wave: 'tom-hi',
+//         url: 'sounds/tom-hi.wav'
+//     },
+//     {
+//         wave: 'tom-low',
+//         url: 'sounds/tom-low.wav'
+//     },
+//     {
+//         wave: 'snare',
+//         url: 'sounds/snare.wav'
+//     },
+//     {
+//          wave: 'rim',
+//         url: 'sounds/rim.wav'
+//     },
+//     {
+//          wave: 'cymbal',
+//         url: 'sounds/cymbal.wav'
+//     },
+//     {
+//          wave: 'shaker',
+//         url: 'sounds/shaker.wav'
+//     },
+//     {
+//          wave: 'snare',
+//         url: 'sounds/snare.wav'
+//     }
+
+// ];
+
+// const steps = 16;
+// const initialCellState = { triggered: false, activated: false };
+// const lineMap = ["BD", "CP", "CH", "OH"];
+// const initialState = [
+//   new Array(16).fill(initialCellState),
+//   new Array(16).fill(initialCellState),
+//   new Array(16).fill(initialCellState),
+//   new Array(16).fill(initialCellState)
+// ];
 
 
+// class Sequencer extends React.Component {
+//     //create state to track all these:
+//     //keep track of the steps pattern
+// //track of current step in the pattern
+// //to check if is currently playing, if play button clicked
+// //(something like: this.state = steps: [' ', ' ' ' ' ' ....'], currentStep: 0, playing: false )
+// //play button should start the clock, stop button should stop the clock
+// //replace setInterval with audioCOntext
+// //schedule the events in loop
+// //handler for play, handlePlay State
+// //to chnge back the state of playing, stop button will setState playing:false
+// //once on play update state of counting steps
+//         constructor() {
+//             super();
+//             this.state= {
+//             steps: [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+//             currentStep: 0,
+//             playing: false
+
+//             };
+
+//             // this.steps = this.createSteps();
+
+//   }
+
+
+//   // createSteps() {
+//   //   let steps = new Array(16);
+//   //   for (let i = 0; i < 16; i++) {
+//   //       console.log(steps);
+//   //     steps[i] = [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false];
+
+//   //   }
+
+//   //   return steps;
+//   // }
+
+//   getScale() {
+//     const tones = [195.995, 220, 233.082, 261.626, 293.665, 311.127, 349.228, 391.995];
+//     return tones
+//   }
+
+//    getWaves() {
+//     //get audio files
+//     const waves = [];
+//     return waves
+//   }
+
+
+// }
+
+// const sequencer = new Sequencer();
+
+
+import React, { useState, useEffect } from "react";
+// import Grid from "./grid";
+import Tone from "tone";
+// import PlayerProvider from "./player-provider";
+// import Bar from "./nav-bar";
+// import PlayButton from "./play-button";
 class Sequencer extends React.Component {
-    //create state to track all these:
-    //keep track of the steps pattern
-//track of current step in the pattern
-//to check if is currently playing, if play button clicked
-//(something like: this.state = steps: [' ', ' ' ' ' ' ....'], currentStep: 0, playing: false )
-//play button should start the clock, stop button should stop the clock
-//replace setInterval with audioCOntext
-//schedule the events in loop
-//handler for play, handlePlay State
-//to chnge back the state of playing, stop button will setState playing:false
-//once on play update state of counting steps
-        constructor() {
+
+
+    constructor() {
             super();
             this.state= {
             steps: [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -75,29 +134,19 @@ class Sequencer extends React.Component {
   }
 
 
-  // createSteps() {
-  //   let steps = new Array(16);
-  //   for (let i = 0; i < 16; i++) {
-  //       console.log(steps);
-  //     steps[i] = [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false];
 
-  //   }
 
-  //   return steps;
-  // }
+render() {
 
-  getScale() {
-    const tones = [195.995, 220, 233.082, 261.626, 293.665, 311.127, 349.228, 391.995];
-    return tones
-  }
+  return (
+    <div>
+      <h1>hello from sequencer</h1>
+      <Grid />
 
-   getWaves() {
-    //get audio files
-    const waves = [];
-    return waves
-  }
-
+    </div>
+  );
 
 }
+};
 
-const sequencer = new Sequencer();
+export default Sequencer;
