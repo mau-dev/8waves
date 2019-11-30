8fc9821e34cafd93bde89f061e36dd7a4c579993
Maria Dimitrijevic <maria.dimitrijevikj@gmail.com>
2019-11-30 15:51:10 +0800

    tone.js player

---
+++ app/javascript/components/player-provider.jsx
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from "react";
+import Tone from "tone";
+
+const PlayerProvider = ({ children }) => {
+  const [player, setPlayer] = useState(null);
+  useEffect(() => {
+    const player = new Tone.Players(
+      {
+        kick: "/sounds/kick.wav",
+        tomHi: "/sounds/tom_hi.wav",
+        tomLow: "/sounds/tom_low.wav",
+        snare: "/sounds/snare.wav",
+        rim: "/sounds/rim.wav",
+        cymbal: "/sounds/cymbal.wav",
+        shaker: "/sounds/shaker.wav",
+        snare: "/sounds/snare.wav"
+      },
+      () => {
+        console.log("buffers loaded");
+        setPlayer(player);
+      }
+    ).toMaster();
+  }, []);
+
+  return children({ player });
+};
+
+export default PlayerProvider;
