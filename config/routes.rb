1eeaec5b7e5f53d25a00126017e088d05004f2e2
Maria Dimitrijevic <maria.dimitrijevikj@gmail.com>
2019-12-04 11:20:32 +0800

    created controller and views for landing page

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,12 @@ Rails.application.routes.draw do
   devise_for :users
   # root "onepage#landing"
    # root :to => redirect('/users/sign_up')
-   root "onepage#index"
+   # root "onepage#index"
+   root "landings#landing"
    get '/getCells' => 'jams#getCell'
 #make the registration page root, and move i=onepage index to /playground
 
   # get 'onepage/index'
-  # get '/playground' => 'onepage#index'
+  get '/playground' => 'onepage#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
