6193b8b40ee06430cb35bf80a18fbc2f1780e50c
Maria Dimitrijevic <maria.dimitrijevikj@gmail.com>
2020-01-01 14:45:16 +0800

    fixed the bug with redirection from the landing page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,11 +6,11 @@ class ApplicationController < ActionController::Base
   protected
 
 
-  # def after_sign_in_path_for(resource)
-  #   '/playground'
-  # end
+  def after_sign_in_path_for(resource)
+    '/playground'
+  end
 
-  #  def after_sign_out_path_for(resource)
-  #   root_path
-  # end
+   def after_sign_out_path_for(resource)
+    root_path
+  end
 end
