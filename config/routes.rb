95b8de287beb98e783afdd042b2acab35687b3e1
Maria Dimitrijevic <maria.dimitrijevikj@gmail.com>
2020-01-02 22:12:16 +0800

    fixed the issue with redirect after sign in, caused by wrong input parameter for the password field

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
    get '/getCells' => 'jams#getCell'
 #make the registration page root, and move i=onepage index to /playground
 
+
   # get 'onepage/index'
   get '/playground' => 'onepage#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
